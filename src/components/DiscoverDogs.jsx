621c6a9ce14cb982d3397227253d0bea95b03636
Nicholas Rios <rios.nicholas@protonmail.com>
2023-10-08 14:48:36 -0400

    Update attribute list as a grid

--- src/components/DiscoverDogs.jsx
+++ src/components/DiscoverDogs.jsx
@@ -2,21 +2,22 @@ import React, { useState } from "react";
 
 const DiscoverDogs = () => {
     const [dogName, setDogName] = useState('Oliver');
+    const attributes = [];
     return (
         <>
             <header className="m-4 p-2">
                 <h1 className="font-extrabold text-6xl">Veni Vici!</h1>
                 <h2 className="text-lg">🦴 Discover some superrr cool dogs! 🐶</h2>
             </header>
-            <main className="m-4 bg-gray-700 p-2 rounded-xl">
+            <main className="m-4 bg-gray-700 p-4 rounded-xl">
                 <h3 className="text-2xl font-bold">{dogName}</h3>
-                <ul className="flex m-2">
+                <ul className="grid md:grid-cols-3 sm:grid-cols-1 m-2">
                     <li className="p-2 m-1 bg-yellow-600 font-semibold rounded-md shadow-sm shadow-gray-700">Attributes here</li>
                     <li className="p-2 m-1 bg-yellow-600 font-semibold rounded-md shadow-sm shadow-gray-700">Attributes here</li>
                     <li className="p-2 m-1 bg-yellow-600 font-semibold rounded-md shadow-sm shadow-gray-700">Attributes here</li>
                 </ul>
-                <img alt={`${dogName} the dog`} src="" />
-                <button>🔀 Discover!</button>
+                <img alt={`${dogName} the dog`} src="" className="m-1"/>
+                <button className="mt-5">🔀 Discover!</button>
             </main>
         </>
     );
