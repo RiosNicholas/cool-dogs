dcae89598467b421ebd522a843c3e887e1a45c25
Nicholas Rios <rios.nicholas@protonmail.com>
2023-10-08 19:19:40 -0400

    Set up API Get request to fetch dog data

--- src/components/DiscoverDogs.jsx
+++ src/components/DiscoverDogs.jsx
@@ -1,23 +1,75 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
-const DiscoverDogs = () => {
-    const [dogName, setDogName] = useState('Oliver');
-    const attributes = [];
+const DiscoverDogs = ({ apiKey, banList, addToBanList }) => {
+    /* HOOKS */
+    const [dogData, setDogData] = useState(null);
+
+    /* FETCH DOG DATA: Breed Name, Breed Group, Temperament Traits, Life Span */
+    const fetchRandomDog = async () => {
+        try {
+            // Fetch response data from API request using .env key
+            const response = await axios.get(`https://api.thedogapi.com/v1/images/search?api_key=${apiKey}`);
+            
+            // Extract Data from API request 
+            if (response.data && response.data.length > 0) {
+                const breedData = response.data[0].breeds[0];
+
+                const name = breedData.name;
+                const breedGroup = breedData.breed_group;
+                const temperament = breedData.temperament.split(', ');
+                const lifeSpan = breedData.life_span;
+
+                setDogData({
+                    name,
+                    breedGroup,
+                    temperament,
+                    lifeSpan,
+                    imageUrl: response.data[0].url,
+                });
+            }
+        } catch (error) {
+            console.error('Error fetching dog data:', error);
+        }
+    };
+
+    // Fetch a random dog's data when the component mounts or when the API key changes
+    useEffect(() => {
+        fetchRandomDog();
+    }, [apiKey]);
+
     return (
-        <div className="flex flex-col items-center justify-center h-screen m-2">
+        <div className="flex flex-col items-center justify-center h-full m-2">
             <header className="p-6">
                 <h1 className="font-extrabold text-6xl">Veni Vici!</h1>
                 <h2 className="text-lg">🦴 Discover some superrr cool dogs! 🐶</h2>
             </header>
             <main className="m-4 bg-gray-700 p-4 rounded-xl">
-                <h3 className="text-2xl font-bold">{dogName}</h3>
-                <ul className="grid md:grid-cols-3 sm:grid-cols-1 m-2">
-                    <li className="p-2 m-1 bg-yellow-600 font-semibold rounded-md shadow-sm shadow-gray-700">Attributes here</li>
-                    <li className="p-2 m-1 bg-yellow-600 font-semibold rounded-md shadow-sm shadow-gray-700">Attributes here</li>
-                    <li className="p-2 m-1 bg-yellow-600 font-semibold rounded-md shadow-sm shadow-gray-700">Attributes here</li>
-                </ul>
-                <img alt={`${dogName} the dog`} src="" className="m-1"/>
-                <button className="mt-5">🔀 Discover!</button>
+                {dogData && (
+                    <>
+                        <h3 className="text-2xl font-bold">{dogData.name}</h3>
+                        <div className="flex justify-center">
+                            <img alt={`${dogData.name} the dog`} src={dogData.imageUrl} className="m-1 w-3/4"/>
+                        </div>
+                        <ul className="grid md:grid-cols-3 sm:grid-cols-1 m-2">
+                            <li className="p-2 m-1 bg-yellow-600 font-semibold rounded-md shadow-sm shadow-gray-700">
+                                {dogData.breedGroup}
+                            </li>
+
+                            {/* Dynamically render separate <li> tags by splitting traits separated by commas*/}
+                            {dogData.temperament.map((trait, index) => (
+                                <li key={index} className="p-2 m-1 bg-yellow-600 font-semibold rounded-md shadow-sm shadow-gray-700">
+                                    {trait}
+                                </li>
+                            ))}
+                   
+                            <li className="p-2 m-1 bg-yellow-600 font-semibold rounded-md shadow-sm shadow-gray-700 overflow-visible">
+                                {dogData.lifeSpan}
+                            </li>
+                        </ul>
+                    </>
+                )}
+                <button className="m-3" onClick={fetchRandomDog}>🔀 Discover!</button>
             </main>
         </div>
     );
