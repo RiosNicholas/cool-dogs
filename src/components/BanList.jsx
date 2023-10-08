a89a64e44090c129e2e4e6dcff4ec7c413ed149b
Nicholas Rios <rios.nicholas@protonmail.com>
2023-10-08 17:20:56 -0400

    Add CSS stying

--- src/components/BanList.jsx
+++ src/components/BanList.jsx
@@ -1,13 +1,17 @@
 const BanList = () => {
-    bannedAttributes = []
+    // bannedAttributes = []
     return (
-       <div>
-        <h2>Ban List</h2>
-        <h3>Select an attribute in your listing to ban it</h3>
-        <ul>
-            <li>Banned attribute goes here*</li>
-        </ul>
-       </div> 
+        <div className="bg-gray-300 h-screen flex justify-center align-middle">
+            <side className="bg-gray-300">
+                <header className="p-3 m-3">
+                    <h2 className="font-bold text-3xl text-black">Ban List</h2>
+                    <h3 className="text-black font-medium">Select an attribute in your listing to ban it</h3>
+                </header>
+                <ul className="flex flex-col p-2">
+                    <li className="p-2 m-1 bg-orange-400 rounded-2xl font-semibold">Attribute*</li>
+                </ul>
+            </side> 
+        </div>
     );
 }
 export default BanList;
